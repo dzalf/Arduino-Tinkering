2e25526d9081c1b53feb5a471093252b99b57d12
dzalf <dzalf@yahoo.com>
2019-01-12 21:34:15 +0000

    Add files via upload

---
+++ 5 - Digispark/Flash/Flash.ino
@@ -0,0 +1,24 @@
+// LED strobe project by Vortecks
+// Connect the MOSFET gate to P0 on Digispark
+
+const byte outPin = 0;
+
+void setup() {
+  pinMode(outPin, OUTPUT);
+  digitalWrite(outPin, LOW);
+  delay(2000);
+}
+
+void loop() 
+{
+  for (int i = 0; i < 2; i++)
+  {
+      digitalWrite(outPin, HIGH);
+      delay(10);
+      digitalWrite(outPin, LOW);
+      delay(100);
+  }
+
+  delay(2500);
+
+}
